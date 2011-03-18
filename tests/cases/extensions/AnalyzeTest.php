085a0d3ef97d98d25db097b318a690eed1d784c0
Marc Schwering <mschwering@weluse.de>
2011-03-18 13:21:46 +0100

    added Analyze Testfile

---
+++ tests/cases/extensions/AnalyzeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace li3_ids\tests\cases\extensions;
+
+use li3_ids\extensions\Analyze;
+
+class AnalyzeTest extends \lithium\test\Unit {
+
+	public function setUp() {}
+
+	public function tearDown() {}
+
+	public function testRun() {}
+}
+
+?>
